2328ea880094e6d8ffd99bc82a56078125dbd918
SergiLomtadze <sergi.lomtadze@gmail.com>
2022-05-09 16:10:03 +0400

    add test project on core6, remove test on framework

---
+++ src/ExadelMentorship.Tests/Features/WeatherTest.cs
@@ -0,0 +1,44 @@
+﻿using ExadelMentorship.BusinessLogic.Exceptions;
+using ExadelMentorship.BusinessLogic.Features.WeatherFeature;
+using ExadelMentorship.BusinessLogic.Models;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExadelMentorship.Tests.Features
+{
+    public class WeatherTest
+    {
+        [Fact]
+        public async Task GetTemperatureByCityName_WhenModelIsValid_ReturnsCity()
+        {
+            //Arrange
+            City city = new City()
+            {
+                Name = "Tbilisi"
+            };
+            Weather weather = new Weather();
+
+            //Act
+            var result = await weather.GetTemperatureByCityName(city);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(result.Temperature.HasValue);
+        }
+
+        [Fact]
+        public async Task GetTemperatureByCityName_WhenModelIsNotValid_ReturnsCity()
+        {
+            //Arrange
+            City city = new City()
+            {
+                Name = "TTT"
+            };
+            Weather weather = new Weather();
+
+            //Act Assert
+            await Assert.ThrowsAsync<NotFoundException>(() =>
+                weather.GetTemperatureByCityName(city));
+        }
+    }
+}
