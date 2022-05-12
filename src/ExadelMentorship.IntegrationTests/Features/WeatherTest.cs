465339e93832b1866369cecfc5bf1e30c92c0bec
SergiLomtadze <sergi.lomtadze@gmail.com>
2022-05-12 21:11:33 +0400

    add integration test

---
+++ src/ExadelMentorship.IntegrationTests/Features/WeatherTest.cs
@@ -0,0 +1,25 @@
+﻿using ExadelMentorship.BusinessLogic.Exceptions;
+using ExadelMentorship.BusinessLogic.Features.WeatherFeature;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExadelMentorship.IntegrationTests.Features
+{
+    public class WeatherTest
+    {
+        [Fact]
+        public async Task GetTemperatureByCityName_WhenCityNameIsNotCorrect_ThrowsException()
+        {
+            //Arrange
+            Weather weather = new Weather(new HttpClient());
+
+            //Act and Assert
+            await Assert.ThrowsAsync<NotFoundException>(() => weather.GetTemperatureByCityName("AAA"));
+        }
+    }
+}
--- src/ExadelMentorship.Tests/Features/WeatherTest.cs
+++ src/ExadelMentorship.Tests/Features/WeatherTest.cs
@@ -9,7 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace ExadelMentorship.Tests.Features
+namespace ExadelMentorship.UnitTests.Features
 {
     public class WeatherTest
     {
