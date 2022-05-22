e7fc88ba419b9eeb4bef0f46bda11e426252c80f
SergiLomtadze <sergi.lomtadze@gmail.com>
2022-05-22 23:21:52 +0400

    add CommandInvoker

---
+++ src/ExadelMentorship.BusinessLogic/CommandInvoker.cs
@@ -0,0 +1,30 @@
+﻿using ExadelMentorship.BusinessLogic.Exceptions;
+using ExadelMentorship.BusinessLogic.Features;
+using ExadelMentorship.BusinessLogic.Features.WeatherFeature;
+using ExadelMentorship.BusinessLogic.Interfaces;
+using ExadelMentorship.BusinessLogic.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExadelMentorship.BusinessLogic
+{
+    public class CommandInvoker
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public CommandInvoker(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public Task Invoke<T>(T command) where T : ICommand
+        {
+            var handler = _serviceProvider.GetRequiredService<ICommandHandler<T>>();
+            return handler.Handle(command);
+        }
+
+    }
+}
