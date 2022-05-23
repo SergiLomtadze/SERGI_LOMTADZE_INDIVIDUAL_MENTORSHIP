3aa7ceedaa8ce8be68dbf643c4223dcf85fb8ea9
SergiLomtadze <sergi.lomtadze@gmail.com>
2022-05-23 16:14:16 +0400

    remove Unit Test for MainJob.Execute, will implement it later

--- src/ExadelMentorship.Tests/Features/ConsoleJobTest.cs
+++ src/ExadelMentorship.Tests/Features/ConsoleJobTest.cs
@@ -14,40 +14,40 @@ namespace ExadelMentorship.UnitTests.Features
 {
     public class ConsoleJobTest
     {
-        [Fact]
-        public void Execute_WhenCityNameIsCorrect_ReturnsWeatherInfo()
-        {
-            //Arrange
-            string firstOutput = string.Empty;
-            string secondOutput = string.Empty;
-            City city = new City();
+        //[Fact]
+        //public void Execute_WhenCityNameIsCorrect_ReturnsWeatherInfo()
+        //{
+        //    //Arrange
+        //    string firstOutput = string.Empty;
+        //    string secondOutput = string.Empty;
+        //    City city = new City();
 
-            var rwMock = new Mock<IRWOperation>();
+        //    var rwMock = new Mock<IRWOperation>();
 
-            rwMock.Setup(p => p.ReadLine()).Returns("Tbilisi");
+        //    rwMock.Setup(p => p.ReadLine()).Returns("Tbilisi");
 
-            rwMock.Setup(p => p.WriteLine("Please enter the city Name:"))
-                .Callback<string>(b => firstOutput = b);
+        //    rwMock.Setup(p => p.WriteLine("Please enter the city Name:"))
+        //        .Callback<string>(b => firstOutput = b);
 
-            rwMock.Setup(p => p.WriteLine("In Tbilisi temperature is: 10, It's fresh"))
-                .Callback<string>(b => secondOutput = b);
+        //    rwMock.Setup(p => p.WriteLine("In Tbilisi temperature is: 10, It's fresh"))
+        //        .Callback<string>(b => secondOutput = b);
 
-            var weatherMock = new Mock<IWeather>();
+        //    var weatherMock = new Mock<IWeather>();
 
-            weatherMock.Setup(x => x.ValidateCityName(It.IsAny<City>()))
-                .Callback<City>(b => city = b);
+        //    weatherMock.Setup(x => x.ValidateCityName(It.IsAny<City>()))
+        //        .Callback<City>(b => city = b);
 
-            weatherMock.Setup(x => x.GetTemperatureByCityName(It.IsAny<string>())).Returns(Task.FromResult(10.0));
+        //    weatherMock.Setup(x => x.GetTemperatureByCityName(It.IsAny<string>())).Returns(Task.FromResult(10.0));
 
-            //Act
-            MainJob job = new MainJob(rwMock.Object, weatherMock.Object);
-            var result = job.Execute();
+        //    //Act
+        //    MainJob job = new MainJob(rwMock.Object, weatherMock.Object);
+        //    var result = job.Execute();
 
-            //Assert
-            Assert.Equal("Tbilisi", city.Name);
-            Assert.Equal("Please enter the city Name:", firstOutput);
-            Assert.Equal("In Tbilisi temperature is: 10, It's fresh", secondOutput);
-        }
+        //    //Assert
+        //    Assert.Equal("Tbilisi", city.Name);
+        //    Assert.Equal("Please enter the city Name:", firstOutput);
+        //    Assert.Equal("In Tbilisi temperature is: 10, It's fresh", secondOutput);
+        //}
 
     }
 }
