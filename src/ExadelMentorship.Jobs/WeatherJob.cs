98f60ad56d9b3893ee78df1148049dbe9fa7ae80
SergiLomtadze <sergi.lomtadze@gmail.com>
2022-06-06 00:29:47 +0400

    delete WeatherJob implementaiton to decrease PR size

--- src/ExadelMentorship.Jobs/WeatherJob.cs
+++ src/ExadelMentorship.Jobs/WeatherJob.cs
@@ -9,36 +9,9 @@ namespace ExadelMentorship.WebApi.Jobs
 {
     public class WeatherJob : IWeatherJob
     {
-        private HistorySettingStorage _historySettingStorage;
-        private IWeatherApiService _weatherApiService;
-        private IWeatherHistorySavingRepository _weatherHistorySavingRepository;
-        public WeatherJob(IOptions<HistorySettingStorage> historySettingStorage,
-            IWeatherApiService weatherApiService,
-            IWeatherHistorySavingRepository weatherHistorySavingRepository)
-        {
-            _historySettingStorage = historySettingStorage.Value;
-            _weatherApiService = weatherApiService;
-            _weatherHistorySavingRepository = weatherHistorySavingRepository;
-        }
-
         public void HistorySaving()
         {
-            foreach (var item in _historySettingStorage.HistorySettings.ToList())
-            {                
-                string[] time = item.ExecutionTime.Split(":");
-
-                RecurringJob.AddOrUpdate(
-                    $"Job_For_{item.City}",
-                    () => Save(item.City),
-                    $"0 {time[1]} {time[0]} * * ?"
-                );
-            }
-        }
-
-        private async Task Save(string cityName)
-        {
-            var temperature = await _weatherApiService.GetTemperatureByCityName(cityName);
-            await _weatherHistorySavingRepository.SaveInDbAsync(cityName, temperature);
+            throw new NotImplementedException();
         }
     }
 }
