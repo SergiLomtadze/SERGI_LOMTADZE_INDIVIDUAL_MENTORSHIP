76f066eb1711e020d174a95ca42a7c5d98fdcc36
SergiLomtadze <sergi.lomtadze@gmail.com>
2022-06-06 16:49:31 +0400

    add HistoryWeather Query,Handler,Response classes

---
+++ src/ExadelMentorship.BusinessLogic/Features/WeatherFeature/HistoryWeather/HistoryWeatherQueryResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExadelMentorship.BusinessLogic.Features.WeatherFeature.HistoryWeather
+{
+    public class HistoryWeatherQueryResponse
+    {
+        public string CityName { get; set; }
+        public double Temperature { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
