007117c84628ff6e876c5e354e3858b975dafb15
SergiLomtadze <sergi.lomtadze@gmail.com>
2022-06-08 10:56:21 +0400

    add Identity Server project part1

---
+++ src/ExadelMentorship.IdentityServer/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.local.json");
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddIdentityServer()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+app.UseIdentityServer();
+
+app.Run();
