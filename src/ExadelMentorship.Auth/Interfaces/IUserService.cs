8a244a1d48cd805c258a345555c28259af5e93d8
SergiLomtadze <sergi.lomtadze@gmail.com>
2022-06-10 15:27:22 +0400

    add Auth as separate web project

--- src/ExadelMentorship.Auth/Interfaces/IUserService.cs
+++
@@ -1,8 +0,0 @@
-﻿using ExadelMentorship.Auth.Models;
-namespace ExadelMentorship.Auth.Interfaces
-{
-    public interface IUserService
-    {
-        Task<AuthenticationModel> GetTokenAsync(TokenRequest request);
-    }
-}
